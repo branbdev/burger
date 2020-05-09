643397c9dded86cfb26e854fdb875a4dd5e917e0
Brandon Bowen <aghoraphobic@gmail.com>
2020-05-08 23:59:58 -0700

    Added and then populated database. Established connection to Express server.

---
+++ db/seeds.sql
@@ -0,0 +1,24 @@
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ('Bacon Avocado Burger', false);
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ('BLT', false);
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ('Double Cheeseburger', false);
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ('Mushroom Swiss Burger', true);
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ('Bleu Cheese Burger', true);
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ('Impossible Burger', true);
--- server.js
+++ server.js
@@ -1,1 +1,22 @@
 const express = require("express");
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+// Serve static content for the app from the 'public' directory
+app.use(express.static(process.cwd() + "/public"));
+
+const exphbs = require("express-handlebars");
+
+// Set Handlebars as the view engine
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Import routes and give the server access to them
+
+var routes = require("./controllers/burgers_controller.js");
+
+app.use("/", routes);
+
+app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
