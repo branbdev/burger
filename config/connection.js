76aebeb11aee3940c8186867f72fc3efa55dd039
Brandon Bowen <aghoraphobic@gmail.com>
2020-05-11 23:04:49 -0700

    Added jaws_db for heroku deployment

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,11 @@
-const mysql = require("mysql");
+var mysql = require("mysql");
 
-let connection;
+// Create the MySQL connection object
+var connection;
 
-if (process.env.PUBLIC_URL) {
+if (process.env.JAWSDB_URL) {
   // DB is JawsDB on Heroku
-  connection = mysql.createConnection(process.env.PUBLIC_URL);
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   // DB is local on localhost
   connection = mysql.createConnection({
